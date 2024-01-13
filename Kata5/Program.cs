9af428b9374e692525de2aa3667d3100ff530f77
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-13 18:03:43 +0100

    Add project files.

---
+++ Kata5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Kata5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool ValidatePin(string pin)
+        {
+            if (pin.Length == 4 || pin.Length == 6)
+            {
+                foreach (char character in pin)
+                {
+                    if (!char.IsDigit(character))
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
